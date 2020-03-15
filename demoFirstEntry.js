04dbc60586fd7bfeac887ccbdb0a093d73648494
marc <krocon@gmail.com>
2020-03-15 11:44:34 +0100

    esm

--- demoFirstEntry.js
+++ demoFirstEntry.js
@@ -1,30 +1,27 @@
+'use strict';
 
-(function(){
-    'use strict';
+import unpackAll from './index.mjs';
 
-    var unpackAll = require('./index.js');
+const archive = 'test/abc (12).rar';
+const options = {
+  targetDir: 'tmp',
+  indexes: [0],
+  forceOverwrite: true,
+  noDirectory: true,
+  quiet: false
+};
 
-    var archive = 'test/abc (12).rar';
-    var options = {
-        targetDir : 'tmp',
-        indexes : [0],
-        forceOverwrite: true,
-        noDirectory: true,
-        quiet: false
-    };
+unpackAll.list(archive, options, (err, files, text) => {
+  if (err) return console.error(err);
 
-    unpackAll.list(archive, options, function (err, files, text) {
-        if (err) return console.error(err);
+  const coverName = files[options.indexes];
+  console.info('coverName', coverName);
 
-        var coverName = files[options.indexes];
-        console.info('coverName', coverName);
+  unpackAll.unpack(archive, options, function (err, files, text) {
+    if (err) return console.error(err);
 
-        unpackAll.unpack(archive, options, function (err, files, text) {
-            if (err) return console.error(err);
+    console.info('files', files);
+    console.info('text', text);
+  });
+});
 
-            console.info('files', files);
-            console.info('text', text);
-        });
-    });
-
-})();
